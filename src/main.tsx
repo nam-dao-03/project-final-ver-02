71630947099a2548457eb46df68a54084b6b75d0
nam-dao <namdao08042003@gmail.com>
2025-01-07 02:34:26 +0700

    fixed fonts and sccs(not done yet)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App.tsx';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import "./assets/scss/main.scss";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
